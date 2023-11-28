74cbae75c2acf6e2f64a24027c3af5bbdf3d3ee8
xiexun6943 <xiexun6943@gmail.com>
2023-11-28 20:44:36 +0800

    调整用户充值 彩金充值

--- coin/ThinkPHP/Extend/Behavior/CheckLangBehavior.class.php
+++ coin/ThinkPHP/Extend/Behavior/CheckLangBehavior.class.php
@@ -22,7 +22,7 @@ class CheckLangBehavior extends Behavior {
     protected $options   =  array(
             'LANG_SWITCH_ON'        => false,   // 默认关闭语言包功能
             'LANG_AUTO_DETECT'      => true,   // 自动侦测语言 开启多语言功能后有效
-            'LANG_LIST'             => 'zh-cn', // 允许切换的语言列表 用逗号分隔
+            'LANG_LIST'             => 'zh-tw', // 允许切换的语言列表 用逗号分隔
             'VAR_LANGUAGE'          => 'l',		// 默认语言切换变量
         );
 
