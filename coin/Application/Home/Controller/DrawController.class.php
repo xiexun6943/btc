7ab3303d82d046bc5eb7c96238e4cf0172aba5e0
YeQiu <jax9951051262@gmail.com>
2024-03-17 16:29:45 +0800

    调整抽奖默认语言为英文

--- coin/Application/Home/Controller/DrawController.class.php
+++ coin/Application/Home/Controller/DrawController.class.php
@@ -81,7 +81,7 @@ class DrawController extends HomeController
             $langArr = array_column($setting, null, 'type');
         }
 
-        $lang=cookie("think_language")?:'zh-tw';
+        $lang=cookie("think_language")?:'en-us';
 
         switch ($lang) {
             case 'zh-cn':
